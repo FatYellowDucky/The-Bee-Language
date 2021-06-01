3b59deb115c0bdf898e40925af1d30aa1ead1cee
FatYellowDucky <82281912+FatYellowDucky@users.noreply.github.com>
2021-06-01 21:46:11 +0200

    version 1.0

--- interpreter.js
+++ interpreter.js
@@ -1,32 +1,111 @@
 const fs=require('fs')
-var fil='';/* λ / læmbʌdɑ / */
-var beta = { /* / bɛta / */
-	storeage:{} /* / stɔridʒ / */
-};for (let i=0;i<process.argv.length;i++){
+var fil='';
+var onion = []
+onion[0] = 0
+onion[1] = 0
+for (let i=0;i<process.argv.length;i++){
 	const n=process.argv[i];
-	if((i>1)&&(i+1!==process.argv.length)){
+	if(i>1&&i+1!==process.argv.length){
 		fil=fil+n+' '
 	}else if((i>1)&&(i+1===process.argv.length)){
 		fil=fil+n
 	}
 }var int=0;var $=false;try{
 	fs.readFileSync(fil).toString()
-	var currstring = '' /* / kəɹɹstɹɪŋ / */
+	var currstring = ''
 }catch(Exception){
-	console.log('[*~*] : '+fil+' is\'nt a directory')
+	console.log('[*~*] : \''+fil+'\' is\'nt a directory')
 	process.exit(1)
-}var λ = fs.readFileSync(fil).toString().split('');λ.forEach(lambda => {
-	if((lambda==='"'||lambda==='\'')&&(!$)){/* $ / ɪstɹɪŋ / inst'ring if in string or not */
-		$=true // use dont adapt text if in a string //
+}var plant = fs.readFileSync(fil).toString().split('');
+plant.forEach(lambda => {
+	if(lambda==='"'||lambda==='\''&&!$){
+		$=true
 		if (lambda === "'") {currstring="'"}
 		else {currstring='"'}
-		λ[int] = ' '+λ[int]+' '
-	}else if((lambda===currstring)&&($)&&λ[int-1]!=='\\') {
-		$=false// tis is functionality of it (with inst'ring[$x="$x" -> " $ x = " $x "])
-		currstring=''/* (without inst'ring[$x="$x" -> " $ x = " $ x "]) */
-		λ[int] = ' '+λ[int]+' '
-	}int++; /* ɪnt */
-});var foo=''; /* fʊː */
-λ.forEach(lambda => {
+		plant[int] = ' '+plant[int]+' '
+	}else if(lambda===currstring&&$&&plant[int-1]!=='\\') {
+		$=false
+		currstring=''
+		plant[int] = ' '+plant[int]+' '
+	}if (lambda==='\r'||lambda==='\n') {
+		plant[int] = ' '
+	}if (lambda==='$'&&!$) {
+		plant[int] = ' $ '
+	}
+	// lets not add if else
+	// without have auto space:
+	// on = and == 
+	if (lambda==='='&&!$) {
+		if (plant[int+1]=='=') {
+			plant[int] = ''
+			plant[int+1] = ' == '
+		}else if (plant[int+1]=='!') {
+			plant[int] = ''
+			plant[int+1] = ' =! '
+		}else{plant[int] = ' = '}
+	}if (lambda===':'&&!$) {
+		plant[int] = ' : '
+	}if ((lambda==='('||lambda===')')&&!$) {
+		if(lambda==='('){onion[0]++}
+		else {onion[1]++}
+		plant[int] = ' '+plant[int]+' '
+	}
+	int++;
+});var foo='';
+plant.forEach(lambda => {
 	foo += lambda
-});console.log(foo)
+});
+if (onion[0]!==onion[1]) {
+	foo = foo.replace(/(\(|\))/g,' ')
+}
+plant = foo.split('');foo=''
+int = 0
+$ = false
+plant.forEach(lambda => {
+	if (plant[int-1]===' '&&plant[int]===' '&&!$) {
+		plant[int-1] = ''
+	}if(lambda==='"'||lambda==='\''&&!$){
+		$=true
+		if (lambda === "'") {currstring="'"}
+		else {currstring='"'}
+	}else if(lambda===currstring&&$&&plant[int-1]!=='\\') {
+		$=false
+		currstring=''
+	}int++;
+});plant.forEach(lambda => {foo += lambda});
+plant = foo.split(' ')
+var carrot = {
+	root:{},
+	core:{},
+};var carrots = 0
+var potato = 0
+$ = false
+for (let tomato = 0; tomato < plant.length; tomato++) {
+	const slot = plant[tomato];
+	if (slot === '$') {
+		if (plant[tomato+2] === '=') {
+			carrot.core[carrots] = plant[tomato+3]
+			carrot.root[plant[tomato+1]] = carrots
+			carrots++
+		}if (plant[tomato+2] === '=!') {
+			try {
+				//console.log(foo) dev
+				//console.log(plant[tomato+3]==='$') dev
+				if (plant[tomato+3]==='$') {
+					potato = carrot.root[plant[tomato+1]]
+					carrot.core[potato] = carrot.core[carrot.root[plant[tomato+4]]]
+				}else{
+					carrot.core[potato] = plant[tomato+3]
+				}
+			} catch (Exception) {
+				carrot.core[carrots] = plant[tomato+3]
+				carrot.root[plant[tomato+1]] = carrots
+				carrots++
+			}
+		}
+	}if (slot==='puts') {
+		if (plant[tomato+1]==='$') {
+			console.log(carrot.core[carrot.root[plant[tomato+2]]])
+		}
+	}
+}// console.log(carrot);console.log(plant) // for development
